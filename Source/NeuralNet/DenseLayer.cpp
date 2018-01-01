c4500c2d3359e043659a34febc83eece8ac4d4ea
CarlHeinz91 <udo.beier@onlinehome.de>
2018-01-01 16:18:30 +0100

    Learning

--- Source/NeuralNet/DenseLayer.cpp
+++ Source/NeuralNet/DenseLayer.cpp
@@ -32,7 +32,7 @@ void DenseLayer::mutate()
 	//Determine mutation chances
 	float chanceOfMutationForMatrix = Random::getRandomFloatBetween(0.f, 0.04f);
 	float chanceOfMutationForBias = Random::getRandomFloatBetween(0.f, 0.04f);
-	float constexpr mutationStrengthConstant = 0.03f;
+	float constexpr mutationStrengthConstant = 0.01f;
 
 	//Mutate matrix
 	for (unsigned int x = 0; x < mMatrix.getSize().x(); ++x)
